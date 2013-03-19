d9609479436cec2465a27e88864e5fd7d3b62566
Harry van Haaren <harryhaaren@gmail.com>
2013-03-19 19:06:13 +0000

    -Using XReparentWindow, still not working

--- widget.cpp
+++ widget.cpp
@@ -3,8 +3,7 @@
 
 #include "widget.h"
 
-// 
-#define FL_LIBRARY
+// #define FL_LIBRARY
 #define FL_INTERNALS
 
 #include <FL/x.H>
@@ -24,8 +23,9 @@ Widget::Widget(void* parentXwindow)
   
   fl_open_display();
   
-  Fl_Window * win = fl_find( (Window)parentXwindow );
+  //Fl_Window * win = fl_find( (Window)parentXwindow );
   
+  /*
   if ( win )
   {
     cout << "found win with Xid" << endl;
@@ -33,8 +33,10 @@ Widget::Widget(void* parentXwindow)
   else
   {
     cout << "creating new window, didnt' find window with XID" << endl;
-    win = new Fl_Window(400,200,"NTK LV2 GUI");
   }
+  */
+  
+  Fl_Window* win = new Fl_Window(400,200,"NTK LV2 GUI");
   
   win->begin();
   Fl_Button* button = new Fl_Button(50,50,80,25,"caption");
@@ -44,14 +46,24 @@ Widget::Widget(void* parentXwindow)
   // this should be commented to stop UI showing up in its own window!
   win->show();
   
+  
+  cout << "reparenting into JALV window " << (Window)parentXwindow  << " from fl window " << fl_xid(win) << endl;
+  
+  //Fl_X * fl_xPtr = Fl_X::set_xid( win, (Window)parentXwindow);
+  XReparentWindow( fl_display, fl_xid( win ), (Window)parentXwindow, 0, 0 );
+  
+  //win->hide();
+  //win->show();
+  
+  
   // hack shown() to return true for embedding
   // this page  (http://www.fltk.org/doc-1.3/osissues.html)  says
   // If not shown() your implementation must call
   // either Fl_X::set_xid() or Fl_X::make_xid()
   
-  //Fl_X * fl_xPtr = Fl_X::set_xid( win, (Window)parentXwindow);
   
-  //Fl::lock();
+  
+  Fl::lock();
   
 }
 
